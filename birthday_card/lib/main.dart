e856572baa2bf2bbd1e8a3d325df32f93f4791c1
RAWANABUHAMMAD <rawan.abuhammad89@gmail.com>
2025-04-16 09:24:19 +0300

    done

---
+++ birthday_card/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/image/birthdayCard.png'),
+              fit: BoxFit.cover, 
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+} 
